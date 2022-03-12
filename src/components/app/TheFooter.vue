2f5c71b7f6cbc93c49708f19978e76d9005108a2
Татьяна Гашева <tg@get-net.ru>
2022-03-12 07:54:01 +0300

    feat: add footer

--- src/components/app/TheFooter.vue
+++ src/components/app/TheFooter.vue
@@ -1,5 +1,7 @@
 <template>
-
+    <div class="footer p-4 bg-dark text-light">
+        <span> © {{(new Date().getFullYear())}} Discovery or its subsidiaries and affiliates. All rights reserved.</span>
+    </div>
 </template>
 
 <script>
@@ -9,5 +11,7 @@ export default {
 </script>
 
 <style scoped>
-
+.footer{
+    font-size: .75rem;
+}
 </style>
