382bc476ea3fdbd6e88ae55dcf1b41a29a3c301c
Татьяна Гашева <tg@get-net.ru>
2022-03-13 07:30:30 +0300

    feat: add pagination to comments tab

---
+++ src/mixins/pagination.mixin.js
@@ -0,0 +1,33 @@
+export default {
+    data() {
+        return {
+            page:                     2,
+            pageSize:                 3,
+            pageCount:                0,
+            paginatedAllItems:        [],
+            paginatedDisplayingItems: [],
+        };
+    },
+    methods: {
+        setupPagination(paginatedAllItems) {
+            this.paginatedAllItems        = [];
+            this.paginatedDisplayingItems = [];
+            this.pageCount                = paginatedAllItems.length;
+
+            if (paginatedAllItems.length > 0 && paginatedAllItems.length <= this.pageSize) {
+                this.paginatedAllItems = paginatedAllItems;
+                this.paginatedDisplayingItems.push(paginatedAllItems);
+                return;
+            }
+
+            while (paginatedAllItems.length > this.pageSize) {
+                this.paginatedDisplayingItems.push(paginatedAllItems.splice(0, this.pageSize));
+            }
+            if (paginatedAllItems.length > 0)
+                this.paginatedDisplayingItems.push(paginatedAllItems);
+        },
+        pageChangeHandler(page) {
+            this.page = page;
+        },
+    },
+};
--- src/views/profile/CommentsView.vue
+++ src/views/profile/CommentsView.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
         <div class="comments_list comments_list--settings d-flex flex-column" v-if="!isLoading">
-            <b-link v-for="comment in comments" class="link--no-decor">
+            <b-link v-for="comment in paginatedDisplayingItems[page-1]" class="link--no-decor">
                 <comment-list-item :author="comment.author"
                                    :date-from="comment.dateFrom"
                                    :comment="comment.comment"/>
             </b-link>
+
+            <div class="pagination-container">
+                <b-pagination
+                    v-model="page"
+                    :total-rows="pageCount"
+                    :per-page="pageSize"
+                    @input="pageChangeHandler"
+                />
+            </div>
         </div>
         <div class="text-center" v-else>
             <b-spinner/>
@@ -15,10 +24,12 @@
 
 <script>
 import CommentListItem from '@/components/recipe/CommentListItem';
+import pagination      from '@/mixins/pagination.mixin';
 
 export default {
     name:       'CommentsView',
     components: {CommentListItem},
+    mixins:     [pagination],
     data() {
         return {
             comments:  null,
@@ -26,7 +37,8 @@ export default {
         };
     },
     async created() {
-        this.comments  = await this.$store.dispatch('getUserComments');
+        this.comments = await this.$store.dispatch('getUserComments', {current: this.page, per: this.pageSize});
+        this.setupPagination(this.comments);
         this.isLoading = false;
     },
     methods: {},
